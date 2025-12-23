e1864ee4bbb2f4e9f1f8a40f28edd3b321585765
Josh Levin <joshhlevinlopez@gmail.com>
2025-12-23 19:46:03 +0800

    dynamic urls

--- CodingPractice/100Days/Day56/app.py
+++ CodingPractice/100Days/Day56/app.py
@@ -1,21 +1,21 @@
-from flask import Flask     
+from flask import Flask, redirect, url_for
 app = Flask(__name__)  
 
 
-@app.route('/')       
-def hello(): 
-    return 'Welcome to Main Page'
+@app.route('/admin')       
+def hello_admin(): 
+    return 'Welcome Admin User'
 
-@app.route('/hello/<name>')       
-def string(name): 
-    return "My name is %s" % name 
+@app.route('/guest/<guest>')       
+def hello_guest(guest): 
+    return "Welcome user %s" % guest  
 
-@app.route('/int/<int:age>')       
-def integer(age): #so naming of functions here are any ra? 
-    return "I am %d years old" % age 
-  
-
-
+@app.route('/user/<name>')       
+def hello_user(name):
+    if name == 'admin': 
+        return redirect(url_for('hello_admin')) 
+    else: 
+        return redirect(url_for('hello_guest', guest=name))
 
 
 app.run(debug=True)
